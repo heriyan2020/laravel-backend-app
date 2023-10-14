fa7f389bcc46b9a82edafd392db15d15192b0974
heriyan2020 <harisetiawan875@gmail.com>
2023-10-15 01:17:07 +0700

    ForgotResetPassword

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -58,5 +58,9 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::verifyEmailView(function () {
             return view('auth.verify');
         });
+
+        Fortify::resetPasswordView(function (Request $request) {
+            return view('auth.reset', ['request' => $request]);
+        });
     }
 }
